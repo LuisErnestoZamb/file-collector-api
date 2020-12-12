e2dd72cae300dee6b0a30c478f85c2a008ac804f
Luis Zambrano <lzambrano@yaraguasoluciones.com>
2020-12-12 03:30:22 -0500

    ENV RUTA_EXTERNA.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   match "/subidaExterna.php" => 'recursos#create', via: [:get, :post]
   match "/renderizar.php" => 'recursos#index', via: [:get, :post]
-  root :to => redirect(ENV['ruta_principal'])
-  get '*path', to: redirect(ENV['ruta_principal'])
+  root to: redirect(ENV['RUTA_EXTERNA'])
+  get '*path', to: redirect(ENV['RUTA_EXTERNA'])
 end
