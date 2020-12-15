7aa646ec5de7809a0e02959eb3cec506ccd09a7d
Luis Zambrano <lzambrano@yaraguasoluciones.com>
2020-12-15 02:53:26 -0500

    Testing subida externa de archivos.

--- app/models/recurso.rb
+++ app/models/recurso.rb
@@ -1,4 +1,5 @@
 class Recurso < ApplicationRecord
   validates_presence_of :cnpnumero
   has_attached_file :carpeta
+  validates_attachment_content_type :carpeta, :content_type => ["binary/octet-stream", "image/jpg", "image/jpeg", "image/png"]
 end
