745362d27e6a400363334f931b9dcd605f195d48
Luis Zambrano <lzambrano@yaraguasoluciones.com>
2020-12-16 00:27:24 -0500

    private access.

--- app/models/recurso.rb
+++ app/models/recurso.rb
@@ -1,5 +1,7 @@
 class Recurso < ApplicationRecord
   validates_presence_of :cnpnumero
   has_attached_file :carpeta
-  do_not_validate_attachment_file_type :carpeta
+  do_not_validate_attachment_file_type :carpeta, :s3_permissions => {
+    :original => :private
+  }
 end
