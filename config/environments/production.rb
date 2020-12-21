79e51f340f02a4b0836dac7dd7632f10ffe7c2a7
Luis Zambrano <lzambrano@yaraguasoluciones.com>
2020-12-21 03:22:43 -0500

    Testing subida externa de archivos.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,5 +105,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.logger = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(STDOUT))
-  config.log_level = :debug
+  config.log_level = :error
 end
